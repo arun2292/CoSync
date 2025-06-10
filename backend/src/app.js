c32ab97185b06059717a6ac36cc515a126b52918
Arun Kumar <arunkumar030520032292@gmail.com>
2025-06-10 11:02:24 +0530

    cors update in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,24 +19,16 @@ const io = connectToSocket(server)
 
 
 app.set("port", (process.env.PORT || 8000))
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "https://cosync-frontend.onrender.com",
+  credentials: true, // needed if you're using cookies/auth headers
+}));
+
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
 app.use("/api/v1/users", userRoutes);
-
-
-
-// const start = async () => {
-//      app.set("mongo_user")
-//      const connectionDb = await mongoose.connect("mongodb+srv://arun15052025:INWw0wiIQLRuazQg@linkupcluster.joteqha.mongodb.net/")
-     
-//      console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
-//         server.listen(app.get("port"), () => {
-//         console.log("LISTENING ON PORT 8000")
-//     });
-
-//  }
 
  const PORT = process.env.PORT || 8000;
 
